d63a28f36720c43213386b8eb72ea0b8a4178dad
aleguill <124363619+aleguill@users.noreply.github.com>
2024-12-16 14:26:35 +0100

    Update data_ingestion.py

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,20 +1,32 @@
 import os
 import requests
+from flask import Flask, jsonify
 from google.cloud import storage
 
-def fetch_data():
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET"])
+
+def fetch_data():  # Obtención de los datos desde la API de tráfico
   url = "http://overpass-api.de/api/interpreter?data=[out:json];node[highway=traffic_signals](50.6,7.0,50.8,7.3);out;"
   response = requests.get(url)
   response.raise_for_status()
   return response.json()
 
-def upload_to_gcs(data, bucket_name, destination_blob):
+def upload_to_gcs(data, bucket_name, destination_blob):  # Carga de los datos a un bucket de GCS
   storage_client = storage.Client()
   bucket = storage_client.bucket(bucket_name)
   blob = bucket.blob(destination_blob)
   blob.upload_from_string(data, content_type = "application/json")
 
 def main(request):
-  data = fetch_data()
-  upload_to_gcs(str(data), os.environ['BUCKET_NAME'], "raw/traffic_data.json")
-  return "Data ingestion complete"
+  try:
+    data = fetch_data()
+    upload_to_gcs(str(data), os.environ['BUCKET_NAME'], "raw/traffic_data.json")
+    return "Data ingestion complete"
+  except Exception as e:
+    return jsonify({"error":str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host = "0.0.0.0", port = port)
