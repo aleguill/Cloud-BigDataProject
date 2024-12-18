79ae0c19e3273f2c8c56cf4c092577c7fe43b5fe
aleguill <124363619+aleguill@users.noreply.github.com>
2024-12-18 18:05:55 +0100

    Code for fetching data

--- src/main.py
+++ src/main.py
@@ -3,26 +3,32 @@ import requests
 from google.cloud import storage
 
 def fetch_data():
-    """Data fetch through Overpass API"""
+    """Obtén datos desde la API de tráfico."""
     url = "http://overpass-api.de/api/interpreter?data=[out:json];node[highway=traffic_signals](40.10,-4.50,41.10,-3.20);out;"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
 def upload_to_gcs(data, bucket_name, destination_blob):
-    """Data upload to GCS"""
+    """Sube datos a un bucket de Google Cloud Storage."""
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob)
     blob.upload_from_string(data, content_type="application/json")
 
 def main(request):
-    """Main fucntion for Google Cloud Functions."""
+    """Función principal para Google Cloud Functions."""
     try:
+        # Obtén los datos de la API
         data = fetch_data()
+
+        # Nombre del bucket
         bucket_name = "grupo12project"
+        # Sube los datos al bucket con un nombre específico
         upload_to_gcs(str(data), bucket_name, "raw/traffic_data.json")
-      
+
+        # Respuesta de éxito
         return {"message": "Data ingestion complete"}, 200
     except Exception as e:
+        # Manejo de errores
         return {"error": str(e)}, 500
